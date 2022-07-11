8228fa5e5d8397ee56f3368c9523d21d48ad8257
jyotsnayall <jyotsnasharma2603@gmail.com>
2022-07-11 23:37:40 +0530

    final server

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code)]
+
+use server::Server;
+use std::env;
+use website_handler::WebsiteHandler;
+
+mod server;
+mod http;
+mod website_handler;
+
+fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC PATH").unwrap_or(default_path);
+    println!("Public Path: {}", public_path);
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run(WebsiteHandler::new(public_path));
+}
